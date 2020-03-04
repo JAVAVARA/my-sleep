b3d0a6d7ffd11645d6ab92e6f8e1bd7587644be1
javavara <javavara@gmail.com>
2020-03-04 17:21:35 +0900

    init

---
+++ my-sleep/src/main/java/net/sleep/MySleepApplication.java
@@ -0,0 +1,13 @@
+package net.sleep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySleepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySleepApplication.class, args);
+	}
+
+}
